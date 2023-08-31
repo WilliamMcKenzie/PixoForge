5ce8479c7eb7265da0cf4b5008016179aa4de521
William McKenzie <williamqmckenzie@gmail.com>
2023-08-31 10:55:52 -0300

    save function

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ drawingContext.imageSmoothingEnabled = false;
 var CELL_SIDE_COUNT = 64;
 var cellPixelLength = canvas.width / CELL_SIDE_COUNT;
 const colorHistory = {};
+var usedColors = ["#000000"]
 var previewHistory = ["//location", "//prev color"];
 let mousePos = { x: undefined, y: undefined };
 setClearCells()
@@ -43,6 +44,10 @@ Object.prototype.getKeyByValue = function (value) {
 // Set default color
 colorInput.value = "#000000"
 
+colorInput.addEventListener("input", () => {
+    usedColors.push(colorInput.value)
+})
+
 //Set canvas bg color 
 drawingContext.fillStyle = "rgba(255, 99, 71, 0)"
 drawingContext.fillRect(0, 0, canvas.width, canvas.height)
@@ -56,6 +61,7 @@ colors.forEach(color => {
         rgbColor = window.getComputedStyle(color).getPropertyValue("background-color")
         var rgb = rgbColor.replace(/^rgba?\(|\s+|\)$/g, '').split(',');
         colorInput.value = rgbToHex(parseInt(rgb[0]), parseInt(rgb[1]), parseInt(rgb[2]))
+        usedColors.push(colorInput.value)
 
         //set selected
         if (document.querySelector('.selectedColor')) document.querySelector('.selectedColor').classList.remove('selectedColor')
@@ -154,8 +160,7 @@ function eyedropperTool() {
     }
 }
 
-function lineTool(e) {
-    console.log("line")
+function selectionTool(e) {
     const canvasBoundingRect = canvas.getBoundingClientRect()
     const cellX = Math.floor((e.clientX - canvasBoundingRect.left) / cellPixelLength)
     const cellY = Math.floor((e.clientY - canvasBoundingRect.top) / cellPixelLength)
@@ -185,17 +190,18 @@ function drawSelection(startX, startY, cellX, cellY, canvasBoundingRect, ctx, st
 
     selectionContext.clearRect(0, 0, canvas.width, canvas.height);
 
+    if (status == "final") {
+        colorHistory[`${cellX2}_${cellY2}`] = colorInput.value
+        console.log(colorHistory)
+    }
+
     ctx.fillStyle = colorInput.value
     ctx.fillRect(Math.floor(startX2), Math.floor(startY2), Math.floor(cellPixelLength), Math.floor(cellPixelLength))
 
     var distancex = 0
     var distancey = 0
 
-    console.log(Math.abs(cellX - cellX2))
-
     for (var i = 0; i < Math.abs(cellX - cellX2); i++) {
-
-        console.log(status)
 
         if (cellX2 < cellX) {
             distancex--
@@ -206,9 +212,14 @@ function drawSelection(startX, startY, cellX, cellY, canvasBoundingRect, ctx, st
         if (status == "final") {
             drawingContext.fillRect(Math.floor(startX + distancex * cellPixelLength), Math.floor(startY2), Math.floor(cellPixelLength), Math.floor(cellPixelLength))
             drawingContext.fillRect(Math.floor(startX + distancex * cellPixelLength), Math.floor(startY), Math.floor(cellPixelLength), Math.floor(cellPixelLength))
+            drawingContext.fillRect(Math.floor(startX2), Math.floor(startY2), Math.floor(cellPixelLength), Math.floor(cellPixelLength))
+            colorHistory[`${cellX + distancex}_${cellY2}`] = colorInput.value
+            colorHistory[`${cellX + distancex}_${cellY}`] = colorInput.value
+            colorHistory[`${cellX}_${cellY}`] = colorInput.value
         } else {
             ctx.fillRect(Math.floor(startX + distancex * cellPixelLength), Math.floor(startY2), Math.floor(cellPixelLength), Math.floor(cellPixelLength))
             ctx.fillRect(Math.floor(startX + distancex * cellPixelLength), Math.floor(startY), Math.floor(cellPixelLength), Math.floor(cellPixelLength))
+            ctx.fillRect(Math.floor(startX2), Math.floor(startY2), Math.floor(cellPixelLength), Math.floor(cellPixelLength))
         }
     }
     for (var i = 0; i < Math.abs(cellY - cellY2); i++) {
@@ -219,14 +230,34 @@ function drawSelection(startX, startY, cellX, cellY, canvasBoundingRect, ctx, st
             distancey++
         }
 
+        if (status == "final") {
+            colorHistory[`${cellX}_${cellY + distancey}`] = colorInput.value
+            colorHistory[`${cellX2}_${cellY + distancey}`] = colorInput.value
+        }
+
         ctx.fillRect(Math.floor(startX), Math.floor(startY + distancey * cellPixelLength), Math.floor(cellPixelLength), Math.floor(cellPixelLength))
         ctx.fillRect(Math.floor(startX2), Math.floor(startY + distancey * cellPixelLength), Math.floor(cellPixelLength), Math.floor(cellPixelLength))
     }
+
+
+    if (status) {
+        var values = colorHistory.getKeyByValue(colorInput.value)
+
+        for (var i = 0; i < values.length; i++) {
+            var curValue = values[i].split("_")
+
+            colorHistory[values[i]] = colorInput.value
+
+            drawingContext.fillStyle = colorInput.value
+            drawingContext.fillRect(Math.floor(curValue[0] * cellPixelLength), Math.floor(curValue[1] * cellPixelLength), Math.floor(cellPixelLength), Math.floor(cellPixelLength))
+        }
+    }
+
     lastSelection = [startX, startY, cellX, cellY, canvasBoundingRect]
 }
 
 function previewAction() {
-    curTool == "pencil" ? document.body.style.cursor = "url('icons/pencil_cursor.png'), auto" : curTool == "bucket" ? document.body.style.cursor = "url('icons/bucket_cursor.png'), auto" : curTool == "eraser" ? document.body.style.cursor = "url('icons/eraser_cursor.png'), auto" : curTool == "eyedropper" ? document.body.style.cursor = "url('icons/eyedropper_cursor.png'), auto" : document.body.style.cursor = "url('icons/favicon.png'), auto";
+    curTool == "pencil" ? document.body.style.cursor = "url('icons/pencil_cursor.png'), auto" : curTool == "bucket" ? document.body.style.cursor = "url('icons/bucket_cursor.png'), auto" : curTool == "eraser" ? document.body.style.cursor = "url('icons/eraser_cursor.png'), auto" : curTool == "eyedropper" ? document.body.style.cursor = "url('icons/eyedropper_cursor.png'), auto" : document.body.style.cursor = "url('icons/selection_cursor.png'), auto";
 
     id = setInterval(() => {
         const canvasBoundingRect = canvasOverlay.getBoundingClientRect()
@@ -253,22 +284,54 @@ function setClearCells() {
     }
 }
 
-function resizeCanvas() {
+function saveCanvas() {
+    var savedCanvas = document.getElementById("savedCanvas")
+    let downloadLink = document.createElement('a');
+    downloadLink.setAttribute('download', 'CanvasAsImage.png');
+
+    savedCanvas.width = CELL_SIDE_COUNT
+    savedCanvas.height = CELL_SIDE_COUNT
+
+    drawPixelArt()
+
+    var dataURL = savedCanvas.toDataURL("image/png");
+    let url = dataURL.replace(/^data:image\/png/, 'data:application/octet-stream');
+    downloadLink.setAttribute('href', url);
+    downloadLink.click();
+}
+
+function drawPixelArt() {
+    var savedCanvas = document.getElementById("savedCanvas")
+    savedCanvas.getContext("2d").clearRect(0, 0, savedCanvas.width, savedCanvas.height)
+
+    usedColors.forEach(color => {
+
+        var values = colorHistory.getKeyByValue(color)
+
+        values.forEach(cur => {
+            cur = cur.split("_")
+
+            var x = cur[0]
+            var y = cur[1]
+
+            savedCanvas.getContext("2d").fillStyle = color
+            savedCanvas.getContext("2d").fillRect(x, y, 1, 1)
+        });
+    });
 }
 
 let id = null;
 function mouseHolding(e) {
-    console.log(curTool)
-    if (curTool != "line") {
+    if (curTool != "selection") {
         id = setInterval(() => curTool == "pencil" ? fillCell() : curTool == "eraser" ? deleteCell() : curTool == "bucket" ? bucketFill() : eyedropperTool(), 1);
     } else {
-        lineTool(e)
+        selectionTool(e)
         selectionContext.clearRect(0, 0, canvas.width, canvas.height);
         canvasSelection.classList.add("selectionActive")
     }
 }
 function mouseRelease() {
-    if (curTool != "line") {
+    if (curTool != "selection") {
         clearInterval(id);
     }
     else {
@@ -328,18 +391,11 @@ function switchEyedropper() {
     document.getElementById("eyedropper").classList.add('selectedTool')
 }
 
-function switchLine() {
-    curTool = "line"
-
-    if (document.querySelector('.selectedTool')) document.querySelector('.selectedTool').classList.remove('selectedTool')
-    document.getElementById("line").classList.add('selectedTool')
-}
-
 function switchSelect() {
-    curTool = "line"
+    curTool = "selection"
 
     if (document.querySelector('.selectedTool')) document.querySelector('.selectedTool').classList.remove('selectedTool')
-    document.getElementById("eyedropper").classList.add('selectedTool')
+    document.getElementById("select").classList.add('selectedTool')
 }
 
 
